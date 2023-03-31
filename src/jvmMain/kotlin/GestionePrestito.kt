7189b4cfd40e455ed9a396fea9a507721cadf883
Ermanno Oliveri <ermitubeyt@gmail.com>
2023-03-31 08:58:59 +0200

    aggiunta commenti

--- src/jvmMain/kotlin/GestionePrestito.kt
+++ src/jvmMain/kotlin/GestionePrestito.kt
@@ -4,7 +4,6 @@ import java.util.concurrent.TimeUnit
 import javax.mail.*
 import javax.mail.internet.InternetAddress
 import javax.mail.internet.MimeMessage
-import kotlin.math.abs
 
 
 /**
@@ -14,6 +13,8 @@ import kotlin.math.abs
  * - verificaPrestito(prestito : Prestito) : Long
  *
  * Da aggiungere dettagli del libro preso in prestito
+ *
+ * @author Ermanno Oliver
  */
 class GestionePrestito {
 
@@ -21,42 +22,71 @@ class GestionePrestito {
     fun verificaPrestito(prestito : Prestito) : Long{
 
         val b = isScaduto(prestito)
-        if (b < 0){
+        return if (b < 0){
             //PRESTITO SCADUTO
-            invioMailScaduto(prestito);
-            return -1;
+            invioMailScaduto(prestito)
+            -1
         } else {
-            return b
+            b
         }
     }
 
-    fun StringToDate(dateString: String?): Date {
+
+    /**
+     * Metodo che trasforma una data in formato string (DD/MM/YYYY)
+     * in un oggetto Date e lo restituisce
+     *
+     * @return Date data
+     */
+    private fun StringToDate(dateString: String?): Date {
         val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
         return dateFormat.parse(dateString)
     }
 
-    fun getLocalDate() : Date {
+    /**
+     * Metodo che restituisce la data attuaòe
+     */
+    private fun getLocalDate() : Date {
         return Calendar.getInstance().time
     }
 
+
+    /**
+     * Prende un oggetto prestito, e valuta se è scaduto o meno
+     * restituisce il numero di giorni rimanente alla sua scadenza,
+     * o se è già avvenuta, il numero di giorni passati
+     *
+     * @param prestito prestito da analizzare
+     * @return la differenza tra la data di fine del prestito e la data attuale
+     */
     fun isScaduto(prestito : Prestito) : Long{
         val diff: Long = StringToDate(prestito.dataFine).time - getLocalDate().time
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS)
     }
 
+
+    /**
+     * Funzione per l'invio di una mail che notifica della scadenza del
+     * prestito di un libro
+     *
+     * @param prestito prestito per il quale inviare la comunicazione
+     */
     fun invioMailScaduto(prestito : Prestito){
 
+        // al momento configurato con mailAlternativa, con configurazione mail
+        // google "volta-alessandria.it" sostituire mailAlternativa in mail
         val destinatario = prestito.persona?.mailAlternativa
-        var testoMail = "Gentile: " + prestito.persona?.nome + " " + prestito.persona?.cognome + "\nIl tuo " +
+
+        //testo della mail da rifare con le info del DB
+        val testoMail = "Gentile: " + prestito.persona?.nome + " " + prestito.persona?.cognome + "\nIl tuo " +
                 "prestito per il libro: " + prestito.isbn + " - copia: " + prestito.idCopia + " del " + prestito.dataInizio + " è scaduto, restituiscilo al più presto." +
                 "\nBiblioteca"
 
+        val username = "ermi1@gmx.com" //username di accesso (email)
+        val password = "Tuboermi4" //password
 
 
-        val username = "ermi1@gmx.com"
-        val password = "Tuboermi4"
-
-
+        //da riconfigurare con autenticazione google
         val properties = Properties().apply {
             put("mail.smtp.host", "mail.gmx.com")
             put("mail.smtp.port", "587")
@@ -76,7 +106,6 @@ class GestionePrestito {
             setSubject("Prestito Libro - Biblioteca")
             setText(testoMail)
         }
-
 
         Transport.send(message)
 
